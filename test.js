f0c5eb87eae13c56cb0ccd343a120d328a117c4a
yonathan <setiadi_y56@yahoo.co.id>
2018-07-10 21:45:28 +0700

    solve endpoint for make a payment

--- test.js
+++
@@ -1,9 +0,0 @@
-const transaction = require('./api/services/transaction')
-
-transaction.insertTransactionTicket(1, 1, 80000, 1)
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.error(err)
-  })
